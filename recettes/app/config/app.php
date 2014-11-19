2a31a3bd621a08acf67f521180a1ee1313c1f382
Antoine Augusti <antoine@augusti.fr>
2014-11-19 14:59:53 +0100

    Alias and Mongo DB config

--- recettes/app/config/app.php
+++ recettes/app/config/app.php
@@ -190,6 +190,7 @@ return array(
 		'URL'               => 'Illuminate\Support\Facades\URL',
 		'Validator'         => 'Illuminate\Support\Facades\Validator',
 		'View'              => 'Illuminate\Support\Facades\View',
+		'Moloquent'         => 'Jenssegers\Mongodb\Model',
 
 	),
 
