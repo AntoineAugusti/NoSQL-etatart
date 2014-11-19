6d81a531d614f78ead20cf0a81d22b5bd0e87538
Antoine Augusti <antoine@augusti.fr>
2014-11-19 15:30:21 +0100

    Enforce values for some attributes of the recipe model

--- recettes/app/Insa/Recipes/Models/Recipe.php
+++ recettes/app/Insa/Recipes/Models/Recipe.php
@@ -4,4 +4,20 @@ use Moloquent;
 
 class Recipe extends Moloquent {
 
+	public function setNoteAttribute($value)
+	{
+		if (! is_int($value) OR $value < 0 OR $value > 10)
+			throw new \InvalidArgumentException("The rating should be between 0 and 10");
+
+		$this->attributes['note'] = $value;
+	}
+
+	public function setTypeAttribute($value)
+	{
+		$allowedValues = ['amuse-gueule', 'dessert', 'plat', 'entree'];
+		if ( ! in_array($value, $allowedValues))
+			throw new \InvalidArgumentException($value." is not a valid type. Possible values are: ".implode('|', $allowedValues));
+			
+		$this->attributes['type'] = $value;
+	}
 }
