ab9459f5b70ae4afd2ed06e422f5e421cc3f9722
Antoine Augusti <antoine@augusti.fr>
2014-11-22 16:32:04 +0100

    Partial view useful for a step when adding a recipe

---
+++ recettes/ressources/views/recipes/partials/addRecipeStep.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.page')
+
+@section('pageTitle')
+	{{ $pageTitle }}
+@stop
+
+@section('content')
+	@include('recipes.partials.breadcrumbSteps')
+
+	<div class="panel panel-default">
+		<div class="panel-body">
+			<h1 class="recipes__big-title"><i class="mdi-maps-restaurant-menu"></i> {{ $pageTitle }}</h1>
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2">
+					<!-- FORM ERRORS -->
+					
+					@include('layouts.partials.formErrors')				
+					<!-- FORM -->
+					{{
+						Form::open([
+							'route' => $postRoute,
+							'class' => 'form-horizontal'
+						]);
+					}}
+						<fieldset>
+							@yield('form')
+							
+							<!-- SUBMIT -->
+							<div class="form-group">
+								<div class="col-lg-10 col-lg-offset-2">
+									<button type="submit" class="btn btn-primary">{{ trans($submitKey) }}</button>
+								</div>
+							</div>
+						</fieldset>
+					{{ Form::close() }}
+				</div>
+			</div>
+		</div>
+	</div>
+@stop
