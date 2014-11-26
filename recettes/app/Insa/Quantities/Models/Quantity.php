a5e75a353904a45d8703103ee1e686d5441ae71e
Antoine Augusti <antoine@augusti.fr>
2014-11-26 19:23:18 +0100

    Move unit and price attributes to the Ingredient model

--- recettes/app/Insa/Quantities/Models/Quantity.php
+++ recettes/app/Insa/Quantities/Models/Quantity.php
@@ -8,25 +8,12 @@ class Quantity extends Moloquent {
 	use PresentableTrait;
 
 	public $timestamps = false;
-	public $fillable = ['unit', 'price', 'quantity'];
+	public $fillable = ['quantity'];
 	protected $presenter = 'Insa\Quantities\Presenters\QuantityPresenter';
-
-	const GRAMMES_LITER = '100g-liter';
-	const KILO = 'kilo';
-	const UNIT = 'unit';
 
 	public function ingredient()
 	{
 		return $this->belongsTo('Insa\Ingredients\Models\Ingredient');
-	}
-
-	public function setUnitAttribute($value)
-	{
-		$allowedValues = $this->getAllowedUnitValues();
-		if ( ! in_array($value, $allowedValues))
-			throw new \InvalidArgumentException($value." is not a valid unit. Possible values are: ".implode('|', $allowedValues));
-			
-		$this->attributes['unit'] = $value;
 	}
 
 	/**
@@ -35,11 +22,6 @@ class Quantity extends Moloquent {
 	 */
 	public function computePrice()
 	{
-		return round($this->price * $this->quantity, 2);
-	}
-
-	public static function getAllowedUnitValues()
-	{
-		return [self::UNIT, self::KILO, self::GRAMMES_LITER];
+		return round($this->ingredient->price * $this->quantity, 2);
 	}
 }
