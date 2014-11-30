22e33793baf14607d13c2cdf55f6382897494556
Antoine Augusti <antoine@augusti.fr>
2014-11-30 19:33:10 +0100

    Create the view to choose the location

---
+++ recettes/ressources/views/locations/choose.blade.php
@@ -0,0 +1,16 @@
+<?php 
+$pageTitle = trans('locations.chooseLocation');
+$postRoute = 'recipes.store';
+$submitKey = 'locations.setThisLocation';
+?>
+@extends('recipes.partials.addRecipeStep')
+
+@section('form')
+	<div class="form-group">
+		<label for="ingredients" class="col-lg-2 control-label">{{ trans('recipes.ingredients') }}</label>
+		<div class="col-lg-10">
+			{{ Form::select('location', $locations, null, ['class' => 'form-control chosen-select']) }}
+			<span class="help-block">{{ trans('locations.chooseLocationHelp') }}</span>
+		</div>
+	</div>
+@stop
