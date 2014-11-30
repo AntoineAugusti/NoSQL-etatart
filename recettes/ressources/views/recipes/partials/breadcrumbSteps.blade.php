3784855d50e8110edd5a6cd89351405319134b8b
Antoine Augusti <antoine@augusti.fr>
2014-11-30 19:31:33 +0100

    Refactor the breadcrumb showing steps when creating a recipe

--- recettes/ressources/views/recipes/partials/breadcrumbSteps.blade.php
+++ recettes/ressources/views/recipes/partials/breadcrumbSteps.blade.php
@@ -1,37 +1,21 @@
+<?php
+$routes = ['recipes.create', 'recipes.ingredients.create', 'recipes.ingredients.quantities.create', 'recipes.location.create'];
+$translations = ['recipes.createRecipe', 'recipes.chooseYourIngredients', 'recipes.chooseIngredientsQuantities', 'recipes.chooseLocation'];
+?>
 <ul class="breadcrumb recipes__breadcrumb-steps">
 	<div class="row">
-		<div class="col-sm-4">
-			@if (Route::currentRouteName() == 'recipes.create')
-				<li class="active">
-					<i class="mdi-navigation-chevron-right"></i> {{ trans('recipes.createRecipe') }}
-				</li>
-			@else
-				<li>
-					{{ trans('recipes.createRecipe') }}
-				</li>
-			@endif
-		</div>
-		<div class="col-sm-4">
-			@if (Route::currentRouteName() == 'recipes.ingredients.create')
-				<li class="active">
-					<i class="mdi-navigation-chevron-right"></i> {{ trans('recipes.chooseYourIngredients') }}
-				</li>
-			@else
-				<li>
-					{{ trans('recipes.chooseYourIngredients') }}
-				</li>
-			@endif
-		</div>
-		<div class="col-sm-4">
-			@if (Route::currentRouteName() == 'recipes.ingredients.quantities.create')
-				<li class="active">
-					<i class="mdi-navigation-chevron-right"></i> {{ trans('recipes.chooseIngredientsQuantities') }}
-				</li>
-			@else
-				<li>
-					{{ trans('recipes.chooseIngredientsQuantities') }}
-				</li>
-			@endif
-		</div>
+		@foreach ($routes as $i => $route)
+			<div class="col-sm-3">
+				@if (Route::currentRouteName() == $route)
+					<li class="active">
+						<i class="mdi-navigation-chevron-right"></i> {{ trans($translations[$i]) }}
+					</li>
+				@else
+					<li>
+						{{ trans($translations[$i]) }}
+					</li>
+				@endif
+			</div>
+		@endforeach
 	</div>
 </ul>
