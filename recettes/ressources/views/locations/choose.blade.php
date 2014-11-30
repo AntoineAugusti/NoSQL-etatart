2e1a25567010e00ac763cc25e88a08569d6cc41d
Antoine Augusti <antoine@augusti.fr>
2014-11-30 19:45:02 +0100

    Correct wrong key name

--- recettes/ressources/views/locations/choose.blade.php
+++ recettes/ressources/views/locations/choose.blade.php
@@ -7,7 +7,7 @@ $submitKey = 'locations.setThisLocation';
 
 @section('form')
 	<div class="form-group">
-		<label for="ingredients" class="col-lg-2 control-label">{{ trans('recipes.ingredients') }}</label>
+		<label for="ingredients" class="col-lg-2 control-label">{{ trans('recipes.location') }}</label>
 		<div class="col-lg-10">
 			{{ Form::select('location', $locations, null, ['class' => 'form-control chosen-select']) }}
 			<span class="help-block">{{ trans('locations.chooseLocationHelp') }}</span>
