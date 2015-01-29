8a0d763a85ba68cb9fc444ec649540750a42d8f3
Antoine Augusti <antoine.augusti@insa-rouen.fr>
2015-01-29 13:29:34 +0100

    Exemple de requêtes pour un ORM

---
+++ livrables/beamer-nosql/code/eloquent.php
@@ -0,0 +1,22 @@
+<?php
+// Création d'un utilisateur
+$data = [
+	'prenom' => 'Antoine',
+	'age'    => 42
+];
+$user = User::($data);
+
+// Sélection des utilisateurs majeurs et articles qu'ils ont écrits
+$users = User::with('articles')
+	->where('age', '>=', 18)
+	->get();
+
+// Suppression des utilisateurs vivant à Paris
+User::where('city', 'Paris')->delete();
+
+// Les derniers articles d'un utilisateur (3ème page)
+$articles = Article::whereUserId($user->id)
+	->latest()
+	->take(10)
+	->skip(20)
+	->get();
