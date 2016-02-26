1f5c4f3e6c2c7ba897afda3a1bb0680a955127dd
Antoine Augusti <antoine.augusti@gmail.com>
2016-02-26 15:06:43 +0100

    Passage de l'exemple du PHP au Java

---
+++ livrables/beamer-nosql/code/eloquent.java
@@ -0,0 +1,22 @@
+<?php
+// Création d'un utilisateur
+Map<String, String> userData = new HashMap<String, String>();
+userData.put("prenom", "Antoine");
+userData.put("augusti", "Augusti");
+User user = User.create(userData);
+
+// Sélection des utilisateurs majeurs et articles qu'ils ont écrits
+ArrayList<User> users = User.with("articles")
+	.where("age", ">=", 18)
+	.get();
+
+// Suppression des utilisateurs vivant à Paris
+User.where("ville", "Paris").delete();
+
+// Les derniers articles d'un utilisateur (3ème page)
+final int NOMBRE_ARTICLES_PAR_PAGE = 10;
+ArrayList<Article> articles = Article.whereUserId(user.getId())
+	.latest()
+	.take(NOMBRE_ARTICLES_PAR_PAGE)
+	.skip(2*NOMBRE_ARTICLES_PAR_PAGE)
+	.get();
